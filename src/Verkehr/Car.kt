521156428c8b5998dd93d4f76cac5de059e402d3
Sven Lang <sven.lang@th-bingen.de>
2017-08-10 10:06:10 +0200

    Autos zählen/ Objekte einbinden

--- src/Verkehr/Car.kt
+++ src/Verkehr/Car.kt
@@ -15,4 +15,31 @@ class Car {
     var wantsToDrive = false
     var delayed = false
     var id = 0
+
+
+    /**
+     * gave the constructor a value with type integer. thought we want to know which car of the list(see: Network.kt) is initialized, driving or delayed
+     * further, i thought to randomize the simulation.. just because it is a simulation and the cars have to know if they are driving or not. not the user(we)?!
+     */
+
+    constructor(id: Int) {
+        this.id = id
+        randomizeWantsToDrive()
+        println("Car: " +id + " object is initialized")
+    }
+
+    fun randomizeWantsToDrive() {
+        wantsToDrive = Random().nextBoolean()
+    }
+
+    /**
+     * next.Boolean for the next input of boolean
+     */
+
+    fun drive() {
+        this.driving = true
+    }
+
+    /** need this for the countCarsOnTheRoad() in Network.kt */
+
 }
--- src/Verkehr/Network.kt
+++ src/Verkehr/Network.kt
@@ -14,4 +14,31 @@ class Network {
     var capacity = 0
     var carsOnTheRoad = 0
     var cars = listOf<Car>()
+
+    constructor(capacity: Int, cars: List<Car>) {
+        this.capacity = capacity
+        this.cars = cars
+        println("Network object is initialized")
+    }
+
+    /**
+     * we need to know how many cars are on the road to compare the with the capacity
+     * idea: a loop give us this value
+     * At First, it didn't work so i have made an function in car.kt for car dring=true
+     * then driving has to be identical true. This for each car so -> carsOnTheRoad increment (++)
+     *
+     */
+
+    fun countCarsOnTheRoad() {
+        carsOnTheRoad = 0
+        for(car in cars) {
+            if(car.driving == true) {
+                carsOnTheRoad++
+            }
+        }
+    }
+
+
+
+
 }
